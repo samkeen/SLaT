5260f69a91fb828c6b097985269594131bfe0e5a
Sam Keen <samkeen@amazon.com>
2019-11-08 15:59:54 -0800

    new release 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='slat',
-    version='0.1.13',
+    version='0.2.0',
     author="Sam Keen",
     author_email="sam.sjk@gmail.com",
     description="Simple Lambda Toolkit",
